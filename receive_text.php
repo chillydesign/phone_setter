5798b52e0ed5eece8230f7b03fc3b71835b051f3
Charles Harvey <harvey.charles@gmail.com>
2021-08-10 10:28:48 +0100

    ditto

--- receive_text.php
+++ receive_text.php
@@ -9,7 +9,3 @@ assign_text($_POST);
 
 $body = 'Code: ' . $_POST['Body'];
 mail('rissel.melissa@gmail.com', 'Text Message Recieved', $body);
-
-
-// bash to see text message json in console
-// cat text_code.txt  | jq . 
---
+++ view_text.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+
+# bash to see text message json in console
+ cat text_code.txt  | jq . 
