f1b7e639db4fb8fc97ee4f3d89c5520904fabacc
Charles Harvey <harvey.charles@gmail.com>
2021-08-10 10:23:48 +0100

    send email on receive text

--- receive_text.php
+++ receive_text.php
@@ -4,3 +4,8 @@ include('connect.php');
 include('api/functions.php');
 
 assign_text($_POST);
+
+
+
+$body = 'Code:' . $_POST['Body'];
+mail('rissel.melissa@gmail.com', 'Text Message Recieved', $body);
