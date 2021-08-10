92aac6d502e3af4809f1c79502edade9d7c579d7
Charles Harvey <harvey.charles@gmail.com>
2021-08-10 10:27:44 +0100

    bash to see text message json in console

--- receive_text.php
+++ receive_text.php
@@ -9,3 +9,7 @@ assign_text($_POST);
 
 $body = 'Code: ' . $_POST['Body'];
 mail('rissel.melissa@gmail.com', 'Text Message Recieved', $body);
+
+
+// bash to see text message json in console
+// cat text_code.txt  | jq . 
